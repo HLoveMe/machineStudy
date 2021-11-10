1922eca7c319c26a993e913b9b9c66f3a9199467
UNITED-IMAGING\zihao.zhu <zihao.zhu_UIH-ST zihao.zhu@uih-surgical.com>
2021-11-10 20:17:03 +0800

    init

--- numPy/index.py
+++ numPy/index.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random.mtrand import random
 
 # Student = np.dtype([('name', 'S20'), ('age', 'i1'), ('marks', 'f4')])
 # data = np.array([('rolf', 21, 75),
@@ -10,9 +11,16 @@ import numpy as np
 # data = np.arange(10, 30, 5)
 # print(type(data))
 
-random =  np.arange(24).reshape(2,3,4)
+# random =  np.arange(24).reshape(2,3,4)
+random = np.random.randint(0,100,(2,3,4))
 print(random,)
 print('11111111111')
 print(random.max(axis=0))
+
+print('222222222222222')
 print(random.max(axis=1))
+print('333333333333')
 print(random.max(axis=2))
+
+
+# print(np.random.randint(0,100,(2,3,4)))
