d07d0ff2bb1cf020330099a43484d130a4d9c815
UNITED-IMAGING\zihao.zhu <zihao.zhu_UIH-ST zihao.zhu@uih-surgical.com>
2021-11-12 09:54:27 +0800

    iris

--- iris_test/iris_test.py
+++ iris_test/iris_test.py
@@ -1,4 +1,5 @@
 
+from math import e
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -15,32 +16,26 @@ train, test = train_test_split(iris_data, test_size=0.2)
 # from sklearn.neighbors import KNeighborsClassifier
 '''
 方式1：
-  1:计算当前点到所有训练集数据的距离
+  1:计算预测数据到所有训练集数据的距离
   2:指定K值 取前K个距离最近的数据
-  3:取出这K个数据的distance 
-  4:看距离哪个最近 最近的为预测数据的类别
+  3:评估最近的K个 计算概率 便可预测数据的类别
 '''
-K = 3
+K = 5
 
 
-def getdistance(item):
-    # print(item.shape)
-    x_0, x_1, x_2, x_3,_ = item
+def forecast_data(item):
+    forecast = item[:-1]
     distance_arr = []
     for data in train:
-        d_0, d_1, d_2, d_3, target = data
-        # print(np.square(d_0-x_0))
-        distance = np.sqrt(
-            np.sum((np.square(d_0-x_0),
-                    np.square(d_1-x_1),
-                    np.square(d_2-x_2),
-                    np.square(d_3-x_3))))
+        target = data[-1]
+        current = data[:-1]
+        distance = np.sqrt(np.sum((current - forecast) ** 2))
         distance_arr.append([distance, target])
-    # print(distance_arr[0])
+
     distance_arr.sort(key=lambda item: item[0], reverse=False)
 
     use_data = distance_arr[0:K]
-    result = [0, 0, 0]
+    result = np.array([0, 0, 0])
     for index in range(K):
         if use_data[index][1] == 0.0:
             result[0] = result[0]+1
@@ -48,12 +43,20 @@ def getdistance(item):
             result[1] = result[1]+1
         elif use_data[index][1] == 2.0:
             result[2] = result[2]+1
+    result = np.stack((np.array([0, 1, 2]), result), axis=1).tolist()
+    result.sort(
+        key=lambda item: item[1], reverse=True)
+    return result[0][0]
 
-    return np.array(result) / K
 
+if __name__ == '__main__':
+    success, fail = 0, 0
+    for item in test:
+        exec = item[-1]
+        forecast = forecast_data(item)
+        if exec == forecast:
+            success += 1
+        else:
+            fail += 1
 
-for index in range(len(test)):
-    item = test[index]
-    forecast = item[-1]
-    result = getdistance(item)
-    print(result,forecast)
+    print('success:', success, 'fail:', fail)
